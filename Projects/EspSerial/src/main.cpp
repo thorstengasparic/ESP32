d16c99f1dfbdd3f9f83411b605b86030bfa08ab4
thorstengasparic <thorsten@gasparic.de>
2023-01-02 22:11:40 +0100

    opto koppler works, I2c on pro mini ok

--- Projects/EspSerial/src/main.cpp
+++ Projects/EspSerial/src/main.cpp
@@ -10,7 +10,7 @@ void setup()
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial2.begin(600, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop() 
--- Projects/MiniSerial/src/main.cpp
+++ Projects/MiniSerial/src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+Adafruit_ADS1115 ads;     /* Use this for the 16-bit version */
+void getValues(void);
+
 SoftwareSerial mySerial(2, 3); // RX, TX
 
 void setup()
@@ -10,11 +16,19 @@ void setup()
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
-  mySerial.begin(115200);
+  mySerial.begin(9600);
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
+
+
 }
 
 void loop() // run over and over
 {
+   getValues();
   while (mySerial.available())
   {
     char c = mySerial.read();
@@ -31,3 +45,32 @@ void loop() // run over and over
     
 }
 
+double volts0Cur, volts1Volt, volts2Ref;
+double measurecurrent, measurevoltage, measurepower;
+unsigned long measureMillis = 0;
+void getValues(void)
+{
+  unsigned long currentMillis = millis();
+  if ((currentMillis- measureMillis) > 1000)
+  {
+    
+    int16_t adc0 = ads.readADC_SingleEnded(0);
+    int16_t adc1 = ads.readADC_SingleEnded(1);
+    int16_t adc2 = ads.readADC_SingleEnded(2);
+    
+    volts0Cur = ads.computeVolts(adc0);
+    volts1Volt = ads.computeVolts(adc1);
+    volts2Ref = ads.computeVolts(adc2);
+    
+    double voltsDelta = volts0Cur -volts2Ref;
+    measurecurrent = voltsDelta*22.14;
+    measurevoltage = volts1Volt*12.0;
+    measurepower =measurevoltage*measurecurrent;
+
+    Serial.print("VCur0="); Serial.print(volts0Cur); Serial.print("\t VRef2="); Serial.print(volts2Ref); Serial.print("\t VVolt1="); Serial.print(volts1Volt);
+    Serial.print("\tV="); Serial.print(measurevoltage); Serial.print("\t A="); Serial.print(measurecurrent);
+    Serial.print("\t P="); Serial.println(measurepower);
+    
+     measureMillis = millis();
+   }
+}
