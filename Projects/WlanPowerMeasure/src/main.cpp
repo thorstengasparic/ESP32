18dae16726cad42d2fc8af52b6a32ffd0df62ce8
thorstengasparic <thorsten@gasparic.de>
2023-04-02 17:58:01 +0200

    ina + ads1114

--- Projects/WlanPowerMeasure/src/main.cpp
+++ Projects/WlanPowerMeasure/src/main.cpp
@@ -1,78 +1,24 @@
-// #include <Adafruit_ADS1X15.h>
-
-// //Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-// Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
-
-// void setup(void)
-// {
-//   Serial.begin(115200);
-//   Serial.println("Hello!");
-
-//   Serial.println("Getting single-ended readings from AIN0..3");
-//   Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-
-//   // The ADC input range (or gain) can be changed via the following
-//   // functions, but be careful never to exceed VDD +0.3V max, or to
-//   // exceed the upper and lower limits if you adjust the input range!
-//   // Setting these values incorrectly may destroy your ADC!
-//   //                                                                ADS1015  ADS1115
-//   //                                                                -------  -------
-//   // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-//   // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-//   // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-//   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-//   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-//   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
-//   if (!ads.begin()) {
-//     Serial.println("Failed to initialize ADS.");
-//     while (1);
-//   }
-//   ads.setGain()
-// }
-
-// void loop(void)
-// {
-//   int16_t adc0, adc1, adc2, adc3;
-//   float volts0, volts1, volts2, volts3;
-
-//   adc0 = ads.readADC_SingleEnded(0);
-//   adc1 = ads.readADC_SingleEnded(1);
-//   adc2 = ads.readADC_SingleEnded(2);
-//   adc3 = ads.readADC_SingleEnded(3);
-
-//   volts0 = ads.computeVo2<<lts(adc0);
-//   volts1 = ads.computeVolts(adc1);
-//   volts2 = ads.computeVolts(adc2);
-//   volts3 = ads.computeVolts(adc3);
-
-//   Serial.println("-----------------------------------------------------------");
-//   Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0); Serial.println("V");
-//   Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(volts1); Serial.println("V");
-//   //Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(volts2); Serial.println("V");
-//   //Serial.print("AIN3: "); Serial.print(adc3); Serial.print("  "); Serial.print(volts3); Serial.println("V");
-
-//   delay(1000);
-// }
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
- #include <EEPROM.h>
- #include <EEPROMProvider.h>
- #include <WlanConnetor.h>
- #include <textreplacement.h>
- #include <table.h>
+ //#include <EEPROM.h>
+ //#include <EEPROMProvider.h>
+ //#include <WlanConnetor.h>
+ //#include <textreplacement.h>
+ //#include <table.h>
+#include <INA226.h>
 
 #define VERSION "1.0"
 
-#define STATUS_LED LED_BUILTIN
+#define SUS_LED LED_BUILTIN
 
+INA226 ina(0x40, &Wire);
+
+void InitINA(void);
+void INA226getValues(void );
 String HttpContentFunction();
-EEPROMProvider* eeprom = NULL;
-WlanConnector *wlanConnector = NULL;
- void getValues(void);
-
+//EEPROMProvider* eeprom = NULL;
+//WlanConnector *wlanConnector = NULL;
 Adafruit_ADS1115 ads;     /* Use this for the 16-bit version */
 
 #if defined(ESP8266)
@@ -84,62 +30,135 @@ Adafruit_ADS1115 ads;     /* Use this for the 16-bit version */
 #else
 #error "This ain't a ESP8266 or ESP32"
 #endif
-
+void InitADS1X15(void );
+void ADS1X15getValues(void );
 void setup() {
   Serial.begin(115200); // Starts the serial communication
   Serial.println("Init...");
 
-  eeprom = new EEPROMProvider(VERSION);
- wlanConnector = new WlanConnector(wifiModePin, STATUS_LED, eeprom);
+  //eeprom = new EEPROMProvider(VERSION);
+ // wlanConnector = new WlanConnector(wifiModePin, STATUS_LED, eeprom);
 
-  wlanConnector->SetCallback(HttpContentFunction);
-
-  if (!ads.begin(ADS1X15_ADDRESS)) {
-    Serial.println("Failed to initialize ADS.");
-    while (1);
-  }
-  
-  eeprom->ArmVersionNumber();  
-  wlanConnector->enableWDT(30);
+  //wlanConnector->SetCallback(HttpContentFunction);
+ //InitINA();
+ InitADS1X15();
+    
+  //eeprom->ArmVersionNumber();  
+  //wlanConnector->enableWDT(30);
 }
 
 void loop() {
-   wlanConnector->Process();    
-    getValues();
+   //wlanConnector->Process();    
+    ADS1X15getValues();
+   //INA226getValues();
 }
 
+unsigned long measureMillisINA226 = 0;
+void INA226getValues(void)
+{
+  unsigned long currentMillis = millis();
+  if ((currentMillis- measureMillisINA226) > 1000)
+  {
+    Serial.println("\nBUS\tSHUNT\tCURRENT\tPOWER");
+      Serial.print(ina.getBusVoltage(), 3);
+    Serial.print("\t");
+    Serial.print(ina.getShuntVoltage_mV(), 3);
+    Serial.print("\t");
+    Serial.print(ina.getCurrent_mA(), 3);
+    Serial.print("\t");
+    Serial.print(ina.getPower_mW(), 3);
+    Serial.print("\t");
+    Serial.print(ina.getShuntVoltage_mV()/0.00466, 3);
+    Serial.println();
+
+    delay(1000);
+      measureMillisINA226 = currentMillis;
+  }
+}
+
+void InitINA()
+{
+    Serial.println(__FILE__);
+
+  Wire.begin();
+  if (!ina.begin() )
+  {
+    Serial.println("could not connect. Fix and Reboot");
+  }
+  Serial.println(ina.setMaxCurrentShunt(20.0, 0.00466));
+  
+}
+
+void InitADS1X15()
+{
+if (!ads.begin(ADS1X15_ADDRESSVCC)) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
+}
 double voltCurrent, refVoltage, voltageExtern, voltageIntern;
 double measurecurrent, measurevoltage, measurepower;
 unsigned long measureMillis = 0;
-void getValues(void)
+double correction = 0.0;
+int16_t adc0=0;
+int16_t adc1=0;
+int16_t adc2=0;
+int16_t adc3=0;
+double iCur0;
+void ADS1X15getValues(void)
 {
-  unsigned long currentMillis = millis();
-  if ((currentMillis- measureMillis) > 1000)
+  int16_t adc0_new = ads.readADC_SingleEnded(0);
+  int16_t adc1_new = ads.readADC_SingleEnded(1);
+  int16_t adc2_new = ads.readADC_SingleEnded(2);
+  int16_t adc3_new = ads.readADC_SingleEnded(3);
+  if(adc2 ==0)
   {
+    adc0 = adc0_new;
+    adc1 = adc1_new;
+    adc2 = adc2_new;
+    adc3 = adc3_new;
+    voltageExtern = ads.computeVolts(adc0);
+    voltageIntern = ads.computeVolts(adc1);
+     voltCurrent = ads.computeVolts(adc2);
+  }
+  else
+  {
+    adc0 = adc0_new;
+    adc1 = adc1_new;
+    adc2 = adc2_new;
+    adc3 = adc3_new;
+  }
+  double voltageExtern_new = ads.computeVolts(adc0);
+  double voltageIntern_new = ads.computeVolts(adc1);
+  double voltCurrent_new = ads.computeVolts(adc2);
+  double delta = 0.01;
+  voltageExtern = (voltageExtern*(1.0-delta)) + (voltageExtern_new * delta);
+  voltageIntern = (voltageIntern*(1.0-delta)) + (voltageIntern_new * delta);
+  voltCurrent = (voltCurrent*(1.0-delta)) + (voltCurrent_new* delta);
+  
+  refVoltage = voltageIntern/2;
+    double deltaV = voltCurrent-refVoltage;
+    if (correction==0.0)
+    {
+      correction = deltaV;
+    }
     
-    int16_t adc0 = ads.readADC_SingleEnded(0);
-    int16_t adc1 = ads.readADC_SingleEnded(1);
-    int16_t adc2 = ads.readADC_SingleEnded(2);
-    int16_t adc3 = ads.readADC_SingleEnded(3);
-    
-    voltCurrent = ads.computeVolts(adc0);
-    refVoltage = ads.computeVolts(adc1);
-    voltageExtern = ads.computeVolts(adc2);
-    voltageIntern = ads.computeVolts(adc3);
-    refVoltage = voltageIntern/2;
-    double delta = voltCurrent-refVoltage +0.01;
-    
-    double iCur0 = (delta)*5.5 ;
+    double iCur0 = (deltaV-correction)*15.0 ;
     
     
-    
+  unsigned long currentMillis = millis();
 
-    Serial.print("VCur="); Serial.print(voltCurrent);Serial.print("\t");
-    Serial.print("VRef="); Serial.print(refVoltage);Serial.print("\t");
-    Serial.print("VExt="); Serial.print(voltageExtern);Serial.print("\t");
-    Serial.print("VInt="); Serial.print(voltageIntern);Serial.print("\t");
-    Serial.print("delta="); Serial.print(delta,2);Serial.print("\t");
-    Serial.print("iCur0="); Serial.print(iCur0,2);Serial.print("\t");
+    
+    
+    
+byte len = 5;
+  Serial.print("Corr="); Serial.print(correction,len);Serial.print("\t");
+    Serial.print("VCur="); Serial.print(voltCurrent,len);Serial.print("\t");
+    Serial.print("VRef="); Serial.print(refVoltage,len);Serial.print("\t");
+    Serial.print("VExt="); Serial.print(voltageExtern*10.0,len);Serial.print("\t");
+    Serial.print("VInt="); Serial.print(voltageIntern,len);Serial.print("\t");
+    Serial.print("delta="); Serial.print(deltaV,len);Serial.print("\t");
+    Serial.print("iCur0="); Serial.print(iCur0,len);Serial.print("\t");
 
 
     /*
@@ -153,8 +172,7 @@ void getValues(void)
     Serial.print("\t P="); Serial.println(measurepower);
     */
    Serial.println("");
-     measureMillis = millis();
-   }
+   measureMillis = millis();
 }
 
 
